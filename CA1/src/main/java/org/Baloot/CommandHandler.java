a841e4c89b86a6dd65e4813f4ecf3310cfb69b4c
Theparia <paria.khoshtab2019@gmail.com>
2023-02-26 18:46:50 +0330

    update commandHandler

--- CA1/src/main/java/org/Baloot/CommandHandler.java
+++ CA1/src/main/java/org/Baloot/CommandHandler.java
@@ -1,7 +1,23 @@
 package org.Baloot;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class CommandHandler {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        while(true) {
+            InputStream input = System.in;
+            BufferedReader buffer = new BufferedReader(new InputStreamReader(input));
+            runCommand(buffer.readLine());
+        }
+
+
     }
+    private static void runCommand(String command) {
+        
+    }
+
+
 }
