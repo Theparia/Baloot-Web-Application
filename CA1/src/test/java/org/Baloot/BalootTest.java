b687e177c21a969d57fa620e404c59a43bfb73cc
Theparia <paria.khoshtab2019@gmail.com>
2023-03-01 14:12:13 +0330

    a rating test added

--- CA1/src/test/java/org/Baloot/BalootTest.java
+++ CA1/src/test/java/org/Baloot/BalootTest.java
@@ -39,25 +39,6 @@ public class BalootTest {
             baloot.addCommodity(commodity);
     }
 
-    @Test
-    public void test_rating_out_of_range_1_to_10(){
-        Response response = baloot.rateCommodity("Theparia", 1, 11);
-        assertFalse(response.isSuccess());
-        assertEquals("Rating Out of Range", response.getData().get("Error").asText());
-    }
-
-    @Test
-    public void test_update_rating_correctly(){
-        baloot.rateCommodity("Theparia", 1, 5);
-        baloot.rateCommodity("parnianf", 1, 7);
-        assertEquals("6.0", baloot.getCommodityById(1).getData().get("rating").asText());
-    }
-//    public void test_commodity_does_not_exist_for_rating(){
-//        Response response = baloot.rateCommodity("1", "1", 9);
-//        assertTrue(response.isSuccess());
-//        assertEquals("Rating Out of Range", response.getData().get("Error").asText());
-//    }
-
     @After
     public void tearDown(){
         baloot = null;
@@ -69,5 +50,32 @@ public class BalootTest {
         providers = null;
         commodities = null;
     }
+
+//    rateCommodity
+    @Test
+    public void test_update_rating_correctly(){
+        baloot.rateCommodity("paria", 1, 5);
+        baloot.rateCommodity("parnian", 1, 7);
+        assertEquals("6.0", baloot.getCommodityById(1).getData().get("rating").asText());
+    }
+
+    @Test
+    public void test_rating_out_of_range_1_to_10(){
+        Response response = baloot.rateCommodity("paria", 1, 11);
+        assertFalse(response.isSuccess());
+        assertEquals("Rating Out of Range", response.getData().get("Error").asText());
+    }
+
+    @Test
+    public void test_replace_rating_for_existing_user(){
+        baloot.rateCommodity("paria", 1, 5);
+        baloot.rateCommodity("paria", 1, 7);
+        assertEquals("7.0", baloot.findCommodityById(1).getUsersRating().get("paria").toString());
+    }
+//    public void test_commodity_does_not_exist_for_rating(){
+//        Response response = baloot.rateCommodity("1", "1", 9);
+//        assertTrue(response.isSuccess());
+//        assertEquals("Rating Out of Range", response.getData().get("Error").asText());
+//    }
 
 }
