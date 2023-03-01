7d62c126a269cce6698263b77dd0c8ff80d8dc74
Theparia <paria.khoshtab2019@gmail.com>
2023-03-01 17:43:18 +0330

    getCommoditiesByCategory tests added

--- CA1/src/test/java/org/Baloot/BalootTest.java
+++ CA1/src/test/java/org/Baloot/BalootTest.java
@@ -16,7 +16,7 @@ public class BalootTest {
     private static Commodity[] commodities;
 
     @BeforeClass
-    public static void preClass(){
+    public static void preClass() {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             users = objectMapper.readValue(new File("src/test/database/users.json"), User[].class);
@@ -29,44 +29,44 @@ public class BalootTest {
     }
 
     @Before
-    public void setUp(){
+    public void setUp() {
         baloot = new Baloot();
-        for(User user : users)
+        for (User user : users)
             baloot.addUser(user);
-        for(Provider provider: providers)
+        for (Provider provider : providers)
             baloot.addProvider(provider);
-        for (Commodity commodity: commodities)
+        for (Commodity commodity : commodities)
             baloot.addCommodity(commodity);
     }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         baloot = null;
     }
 
     @AfterClass
-    public static void postClass(){
+    public static void postClass() {
         users = null;
         providers = null;
         commodities = null;
     }
 
-//    rateCommodity
+    //    rateCommodity
     @Test
-    public void test_update_rating_correctly(){
+    public void test_update_rating_correctly() {
         baloot.rateCommodity("paria", 1, 5);
         assertEquals("6.25", baloot.getCommodityById(1).getData().get("rating").asText());
     }
 
     @Test
-    public void test_rating_out_of_range_1_to_10(){
+    public void test_rating_out_of_range_1_to_10() {
         Response response = baloot.rateCommodity("parnian", 1, 11);
         assertFalse(response.isSuccess());
         assertEquals("Rating Out of Range", response.getData().get("Error").asText());
     }
 
     @Test
-    public void test_replace_rating_for_existing_user(){
+    public void test_replace_rating_for_existing_user() {
         baloot.rateCommodity("paria", 1, 5);
         baloot.rateCommodity("paria", 1, 7);
         assertEquals("7.0", baloot.findCommodityById(1).getUsersRating().get("paria").toString());
@@ -74,19 +74,32 @@ public class BalootTest {
     }
 
     @Test
-    public void test_commodity_does_not_exist_for_rating(){
+    public void test_commodity_does_not_exist_for_rating() {
         Response response = baloot.rateCommodity("paria", 4, 9);
         assertFalse(response.isSuccess());
         assertEquals("Commodity does not exist.", response.getData().get("Error").asText());
     }
 
     @Test
-    public void test_user_does_not_exist_for_rating(){
+    public void test_user_does_not_exist_for_rating() {
         Response response = baloot.rateCommodity("amin", 1, 5);
         assertFalse(response.isSuccess());
         assertEquals("User does not exist.", response.getData().get("Error").asText());
     }
 
-//    getCommoditiesByCategory
+    //    getCommoditiesByCategory
+    @Test
+    public void test_get_commodities_by_category_correctly() {
+        Response response = baloot.getCommoditiesByCategory("Phone");
+        assertTrue(response.isSuccess());
+        assertEquals("[{\"id\":3,\"name\":\"iPhone 5s\",\"providerId\":2,\"price\":1000.0,\"categories\":[\"Phone\",\"Technology\"],\"rating\":9.5}]",
+                response.getData().get("commoditiesListByCategory").toString());
+    }
 
+    @Test
+    public void test_get_commodities_by_non_existing_category() {
+        Response response = baloot.getCommoditiesByCategory("Art");
+        assertTrue(response.isSuccess());
+        assertEquals("", response.getData().get("commoditiesListByCategory").toString());
+    }
 }
