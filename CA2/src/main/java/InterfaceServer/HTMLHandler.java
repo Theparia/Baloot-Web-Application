89bd8f4e877a010197907c85c15345b02dc91c2e
parnianf <pr.fazel@gmail.com>
2023-03-15 13:05:55 -0400

    User + payment + add_user + commodity

--- CA2/src/main/java/InterfaceServer/HTMLHandler.java
+++ CA2/src/main/java/InterfaceServer/HTMLHandler.java
@@ -1,15 +1,20 @@
 package InterfaceServer;
 
-import Baloot.Baloot;
+import Baloot.*;
 import Baloot.Commodity;
+import Baloot.Exceptions.CommodityOutOfStock;
+import Baloot.Exceptions.UserNotFound;
 import Database.Database;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import org.jetbrains.annotations.NotNull;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
+import org.jsoup.nodes.Element;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 class CommoditiesListHandler implements Handler {
 
@@ -54,4 +59,174 @@ class ProviderPageHandler implements Handler {
 
     }
 }
+
+class UserPageHandler implements Handler{
+    private Baloot baloot;
+    public UserPageHandler(Baloot baloot){
+        this.baloot = baloot;
+    }
+    @Override
+    public void handle(@NotNull Context ctx) throws Exception {
+
+        Document doc = Jsoup.parse(new File("CA2/src/main/resources/Templates/User.html"), "UTF-8");
+        String userId = ctx.pathParam("user_id");
+        try {
+            User user = baloot.findUserByUsername(userId);
+            doc.getElementById("username").text("Username: " + user.getUsername());
+            doc.getElementById("email").text("Email: " + user.getEmail());
+            doc.getElementById("birthDate").text("Birth Date: " + user.getBirthDate());
+            doc.getElementById("address").text(user.getBirthDate());
+            doc.getElementById("credit").text("Credit: " + String.valueOf(user.getCredit()));
+            Element paymentForm = doc.select("form").first();
+            paymentForm.attr("action", "/payment");
+
+            doc.getElementById("form_payment_userId").attr("value", userId);
+
+//Add to buyList part:
+            Element table = doc.select("table").first();
+            List<Commodity> buyList = baloot.findUserByUsername(userId).getBuyList();
+            for (Commodity commodity : buyList) {
+                Element row = getHtmlTableRow(commodity);
+                Element form = constructHTMLForm(commodity, row);
+                Element input = constructHTMLInputTag(commodity);
+                Element button = new Element("button");
+                Element formCell = new Element("td");
+                button.attr("type", "submit");
+                button.text("Remove");
+                form.appendChild(input);
+                form.appendChild(button);
+                formCell.appendChild(form);
+                row.appendChild(formCell);
+                table.appendChild(row);
+            }
+
+        } catch(Exception e){
+            ctx.redirect("/403");
+        }
+        ctx.contentType("text/html");
+        ctx.result(doc.toString());
+    }
+
+
+
+    private Element constructHTMLForm(Commodity commodity, Element row){
+        Element linkCell = new Element("td");
+        Element link = new Element("a");
+        link.attr("href", "/commodities/" + commodity.getId());
+        link.text("Link");
+        linkCell.appendChild(link);
+        row.appendChild(linkCell);
+        Element form = new Element("form");
+        form.attr("action", "");
+        form.attr("method", "POST");
+        return form;
+    }
+
+    private Element constructHTMLInputTag(Commodity commodity){
+        Element input = new Element("input");
+        input.attr("type", "hidden");
+        input.attr("name", "commodityId");
+        input.attr("value", String.valueOf(commodity.getId()));
+        return input;
+    }
+
+    private Element getHtmlTableRow(Commodity commodity){
+        Element row = new Element("tr");
+        row.append("<th>" + commodity.getId() + "</th>");
+        row.append("<th>" + commodity.getName() + "</th>");
+        row.append("<th>" + commodity.getProviderId() + "</th>");
+        row.append("<th>" + commodity.getPrice() + "</th>");
+        row.append("<th>" + commodity.getCategories() + "</th>");
+        row.append("<th>" + commodity.getRating() + "</th>");
+        row.append("<th>" + commodity.getInStock() + "</th>");
+        return row;
+    }
+}
+
+
+
+class UserPaymentHandler implements Handler{
+    private Baloot baloot;
+    public UserPaymentHandler(Baloot baloot){
+        this.baloot = baloot;
+    }
+    @Override
+    public void handle(@NotNull Context ctx) {
+        try {
+            String userId = ctx.formParam("form_payment_userId");
+            System.out.println("--> username: "+ userId);
+            baloot.finalizePayment(userId);
+            ctx.redirect("/users/" + userId);
+        } catch (UserNotFound | CommodityOutOfStock exception) {
+            ctx.redirect("/404");
+        }
+    }
+}
+
+
+class CommodityPageHandler implements Handler{
+    private Baloot baloot;
+    public CommodityPageHandler(Baloot baloot){
+        this.baloot = baloot;
+    }
+    @Override
+    public void handle(@NotNull Context ctx) throws IOException {
+        Document doc = Jsoup.parse(new File("CA2/src/main/resources/Templates/Commodity.html"), "UTF-8");
+        String commodityId = ctx.pathParam("commodity_id");
+
+        try {
+            Commodity commodity = baloot.findCommodityById(Integer.valueOf(commodityId));
+            doc.getElementById("id").text("Id: " + commodity.getId());
+            doc.getElementById("name").text("Name: " + commodity.getName());
+            doc.getElementById("providerId").text("Provider Id: " + commodity.getProviderId());
+            doc.getElementById("price").text("Price: " + commodity.getPrice());
+            doc.getElementById("categories").text("Categories: " + commodity.getCategories());
+            doc.getElementById("rating").text("Rating: " + commodity.getRating());
+            doc.getElementById("inStock").text("In Stock: " + commodity.getInStock());
+
+            Element form = doc.getElementById("add_to_buyList");
+            form.attr("formaction", "/addToBuyList/" + commodityId);
+
+        } catch(Exception e){
+            ctx.redirect("/403");
+        }
+        ctx.contentType("text/html");
+        ctx.result(doc.toString());
+
+    }
+}
+class addToBuyListHandler implements Handler{
+    private Baloot baloot;
+    public addToBuyListHandler(Baloot baloot){
+        this.baloot = baloot;
+    }
+    @Override
+    public void handle(@NotNull Context ctx) {
+        try {
+            String commodityId = ctx.pathParam("commodityId");
+            String userId = ctx.formParam("userId");
+            baloot.addToBuyList(userId, Integer.valueOf(commodityId));
+            System.out.println("username: " + userId + " commodity: " + commodityId);
+            ctx.redirect("/200");
+        }catch (Exception e){
+            ctx.redirect("/404");
+        }
+        ctx.contentType("text/html");
+
+    }
+}
+
+class StatusCodePageHandler implements Handler {
+    private String code;
+    public StatusCodePageHandler(String code){
+        this.code = code;
+    }
+    @Override
+    public void handle(@NotNull Context ctx) throws Exception {
+        Document doc = Jsoup.parse(new File("CA2/src/main/resources/Templates/" + code + ".html"), "UTF-8");
+        ctx.contentType("text/html");
+        ctx.result(doc.toString());
+    }
+}
+
 
