343b2270c83806a76f8cf81647c8817c65bd26c4
Theparia <paria.khoshtab2019@gmail.com>
2023-05-02 19:52:59 +0330

    history and cart tables added to user page

--- CA5/frontend/src/pages/user/User.js
+++ CA5/frontend/src/pages/user/User.js
@@ -3,7 +3,7 @@ import Header from "../../components/Header/Header.js";
 import {useParams} from "react-router-dom";
 import Footer from "../../components/Footer/Footer.js";
 import "./User.css"
-import {addCredit, getBuyList, getUser} from "../../apis/UserRequest.js";
+import {addCredit, getBuyList, getPurchasedList, getUser} from "../../apis/UserRequest.js";
 
 
 const UserInfo = () => {
@@ -21,19 +21,19 @@ const UserInfo = () => {
         return (
             <div className="info font">
                 <div>
-                    <img src="/images/user.png" />
+                    <img src="/images/user.png"/>
                     {user.username}
                 </div>
                 <div>
-                    <img src="/images/email.png" />
+                    <img src="/images/email.png"/>
                     {user.email}
                 </div>
                 <div>
-                    <img src="/images/date.png" />
+                    <img src="/images/date.png"/>
                     {user.birthDate}
                 </div>
                 <div>
-                    <img src="/images/address.png" />
+                    <img src="/images/address.png"/>
                     {user.address}
                 </div>
             </div>
@@ -42,13 +42,18 @@ const UserInfo = () => {
 
     const Credit = () => {
         const [amount, setAmount] = useState("");
-        function handleAddCredit(e){
+
+        function handleAddCredit(e) {
             e.preventDefault();
             addCredit(user.username, {"amount": amount})
                 .then(response => {
                     console.log(response.data);
-                    console.log("NEW CREDIT : " + user.credit);
-                    setUser({ ...user, credit: parseFloat(user.credit) + parseFloat(amount) });
+                    getUser(sessionStorage.getItem('username')).then(response => {
+                            setUser(response.data)
+                        }
+                    )
+                    console.log("NEW CREDIT : " + user.credit); //TODO: WHAT?
+                    // setUser({ ...user, credit: parseFloat(user.credit) + parseFloat(amount) });
                     setAmount("");
                 }).catch((error) => alert(error.response.data));
         }
@@ -56,12 +61,12 @@ const UserInfo = () => {
         return (
             <div className="credit">
                 <div id="font">
-                    <img src="/images/dollar.png" />
+                    <img src="/images/dollar.png"/>
                     {user.credit}
                 </div>
                 <div>
                     <input className="btn-font" id="amount-input" placeholder="$Amount" value={amount}
-                           onChange={(event)=>setAmount(event.target.value)}/>
+                           onChange={(event) => setAmount(event.target.value)}/>
                 </div>
                 <div>
                     <button className="btn btn-font" type="submit"
@@ -75,209 +80,173 @@ const UserInfo = () => {
 
     return (
         <div className="user-section">
-            <Info />
-            <Credit />
+            <Info/>
+            <Credit/>
+        </div>
+    )
+}
+
+const InCart = ({commodityId}) => {
+    return (
+        <div className="in-cart-btn">
+            <a href="">-</a>
+            <div> 1</div>
+            <a href="">+ </a>
+        </div>
+    )
+}
+
+const CommodityTableRow = ({commodity, tableType}) => {
+    return (
+        <div className="table-row">
+            <div className="col-header-font">
+                <img src={commodity.image}/>
+            </div>
+            <div className="col-font col-header-font">
+                {commodity.name}
+            </div>
+            <div className="col-font col-header-font">
+                {commodity.categories}
+            </div>
+            <div className="col-font col-header-font">
+                {commodity.price}
+            </div>
+            <div className="col-font col-header-font">
+                {commodity.providerId}
+            </div>
+            <div className="col-rating-font col-header-font">
+                {commodity.rating}
+            </div>
+            <div className="col-in-stock-font col-header-font">
+                {commodity.inStock}
+            </div>
+            <div className="col-header-font">
+                {tableType === "Cart" && <InCart/>}
+                {tableType === "History" && commodity.quantity}
+            </div>
+        </div>
+    )
+}
+
+const CommodityTableHeader = ({tableType}) => {
+    return (
+        <div className="table-row">
+            <div className="col-header-font">
+                Image
+            </div>
+            <div className="col-header-font">
+                Name
+            </div>
+            <div className="col-header-font">
+                Categories
+            </div>
+            <div className="col-header-font">
+                Price
+            </div>
+            <div className="col-header-font">
+                Provider ID
+            </div>
+            <div className="col-header-font">
+                Rating
+            </div>
+            <div className="col-header-font">
+                In Stock
+            </div>
+            <div className="col-header-font">
+                {tableType === "Cart" && "In Cart"}
+                {tableType === "History" && "Quantity"}
+            </div>
+        </div>
+    )
+}
+
+const CommoditiesTable = ({tableType, commoditiesList}) => {
+    return(
+        <div className="commodities-table">
+            <CommodityTableHeader tableType={tableType}/>
+            {
+                commoditiesList.map((item, index) => (
+                    <CommodityTableRow key={index} commodity={item} tableType={tableType}/>
+                ))
+            }
         </div>
     )
 }
 
 const BuyList = () => {
-    const [buyList, setBuyList] = useState({});
+    const [buyList, setBuyList] = useState([]);
 
     useEffect(() => {
         getBuyList(sessionStorage.getItem('username')).then(response => {
-            const commodities = [];
-
-            console.log("&&&  " + Object.entries(response.data).map(([key, value]) => {
-                return {...key, quantity: value};
-            }));
-
-            const keys = Object.keys(response.data);
-            for(let i in keys) {
-                // console.log(keys[i].name + " %%%%% " + response.data[keys[i]]);
-                commodities.push({...keys[i], quantity: response.data[keys[i]]});
-            }
-            setBuyList(commodities);
+            setBuyList(response.data);
         })
     }, [])
 
-    return(
-        <>
+
+    const PayNowButton = () => { //TODO: ZERO SIZE
+        return (
+            <div className="pay-btn">
+                <button className="btn btn-font">
+                    Pay Now!
+                </button>
+            </div>
+        )
+    }
+
+    const CartLogo = () => {
+        return(
             <div>
-                <img src="/images/cart.png" />
+                <img src="/images/cart.png"/>
                 <span className="topic-font">Cart</span>
             </div>
-            <div className="cart-table">
-                <div className="table-row">
-                    <div className="col-header-font">
-                        Image
-                    </div>
-                    <div className="col-header-font">
-                        Name
-                    </div>
-                    <div className="col-header-font">
-                        Categories
-                    </div>
-                    <div className="col-header-font">
-                        Price
-                    </div>
-                    <div className="col-header-font">
-                        Provider ID
-                    </div>
-                    <div className="col-header-font">
-                        Rating
-                    </div>
-                    <div className="col-header-font">
-                        In Stock
-                    </div>
-                    <div className="col-header-font">
-                        In Cart
-                    </div>
-                </div>
-                <div className="table-row">
-                    <div className="col-header-font">
-                        <img src="/images/product.png" />
-                    </div>
-                    <div className="col-font col-header-font">
-                        Galaxy S21
-                    </div>
-                    <div className="col-font col-header-font">
-                        Technology, Phone
-                    </div>
-                    <div className="col-font col-header-font">
-                        $21000000
-                    </div>
-                    <div className="col-font col-header-font">
-                        1234
-                    </div>
-                    <div className="col-rating-font col-header-font">
-                        8.3
-                    </div>
-                    <div className="col-in-stock-font col-header-font">
-                        17
-                    </div>
-                    <div className="col-header-font">
-                        <div className="in-cart-btn">
-                            <a href="">-</a>
-                            <div> 1</div>
-                            <a href="">+ </a>
-                        </div>
-                    </div>
-                </div>
-                <div className="pay-btn">
-                    <button className="btn btn-font">
-                        Pay Now!
-                    </button>
-                </div>
+        )
+    }
+
+    return (
+        <>
+            <CartLogo/>
+            <CommoditiesTable tableType={"Cart"} commoditiesList={buyList}/>
+            <PayNowButton/>
+        </>
+    )
+}
+
+const PurchasedList = () => {
+    const [purchasedList, setPurchasedList] = useState([]);
+
+    useEffect(() => {
+        getPurchasedList(sessionStorage.getItem('username')).then(response => {
+            setPurchasedList(response.data);
+        })
+    }, [])
+
+    const HistoryLogo = () => {
+        return(
+            <div>
+                <img src="/images/history.png"/>
+                <span className="topic-font">History</span>
             </div>
+        )
+    }
+
+    return(
+        <>
+            <HistoryLogo/>
+            <CommoditiesTable tableType={"History"} commoditiesList={purchasedList}/>
         </>
     )
 }
 
 const UserBody = () => {
-    return(
+    return (
         <main>
             <UserInfo/>
             <BuyList/>
+            <PurchasedList/>
         </main>
     )
 }
 
-const Test = () => {
-    return (
-        <main>
-            <div>
-
-                <div>
-                    <div>
-                        <img src="/images/history.png" />
-                            <span className="topic-font">History</span>
-                    </div>
-                    <div className="cart-table">
-                        <div className="table-row">
-                            <div className="col-header-font">
-                                Image
-                            </div>
-                            <div className="col-header-font">
-                                Name
-                            </div>
-                            <div className="col-header-font">
-                                Categories
-                            </div>
-                            <div className="col-header-font">
-                                Price
-                            </div>
-                            <div className="col-header-font">
-                                Provider ID
-                            </div>
-                            <div className="col-header-font">
-                                Rating
-                            </div>
-                            <div className="col-header-font">
-                                In Stock
-                            </div>
-                            <div className="col-header-font">
-                                Quantity
-                            </div>
-                        </div>
-                        <div className="table-row">
-                            <div className="col-header-font">
-                                <img src="/images/spaghetti.png" />
-                            </div>
-                            <div className="col-font col-header-font">
-                                Mom’s Spaghetti
-                            </div>
-                            <div className="col-font col-header-font">
-                                Food
-                            </div>
-                            <div className="col-font col-header-font">
-                                $60000
-                            </div>
-                            <div className="col-font col-header-font">
-                                313
-                            </div>
-                            <div className="col-rating-font col-header-font">
-                                10
-                            </div>
-                            <div className="col-in-stock-font col-header-font">
-                                0
-                            </div>
-                            <div className="col-font col-header-font">
-                                3
-                            </div>
-                        </div>
-                        <div className="table-row">
-                            <div className="col-header-font">
-                                <img src="/images/microphone.png" />
-                            </div>
-                            <div className="col-font col-header-font">
-                                Dre’s Microphone
-                            </div>
-                            <div className="col-font col-header-font">
-                                Technology
-                            </div>
-                            <div className="col-font col-header-font">
-                                $4200000
-                            </div>
-                            <div className="col-font col-header-font">
-                                4321
-                            </div>
-                            <div className="col-rating-font col-header-font">
-                                8.5
-                            </div>
-                            <div className="col-in-stock-font col-header-font">
-                                22
-                            </div>
-                            <div className="col-font col-header-font">
-                                1
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </main>
-    )
-}
 
 const User = () => {
     const {username} = useParams();
